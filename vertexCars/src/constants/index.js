437b544083b15a9ff39c5f7cbb6af5735424ecb0
Anay21013 <108222985+Anay21013@users.noreply.github.com>
2023-04-25 04:39:29 +0530

    Create index.js

---
+++ vertexCars/src/constants/index.js
@@ -0,0 +1,187 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star, review1, review2, review3  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "feedback",
+    title: "Reviews",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "Repairs",
+    content:
+      "Trust our expert technicians to give your car the best care it deserves.",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "Insurance",
+    content:
+      "Protect your car and your peace of mind with our comprehensive insurance options.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "Car Sales",
+    content:
+      "Find the car of your dreams with our wide selection of new and used vehicles.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Wonderful!!!!Good Team, Professional team for each & every detailed needs related to cars.Arranged My choices Fortuner within a week.",
+    name: "Praveen Aggarwal",
+    title: "20 reviews",
+    img: review1,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Amazing services, highly recommended. Mohit is highly professional and take care of each and every document required.",
+    name: "Mohit Bansal",
+    title: "1 review",
+    img: review2,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "I purchase all my cars from vertex cars. They surely give you best deals possible.You can surely rely on them.",
+    name: "Shiv Chawla",
+    title: "1 review",
+    img: review3,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "Happy clients",
+    value: "1000+",
+  },
+  {
+    id: "stats-2",
+    title: "Cars sold",
+    value: "3000+",
+  },
+];
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.hoobank.com/content/",
+      },
+      {
+        name: "How it Works",
+        link: "https://www.hoobank.com/how-it-works/",
+      },
+      {
+        name: "Create",
+        link: "https://www.hoobank.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.hoobank.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.hoobank.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.hoobank.com/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.hoobank.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.hoobank.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.hoobank.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.hoobank.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.hoobank.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.hoobank.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://instagram.com/vertexcars?igshid=YmMyMTA2M2Y=",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/vertexcars/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/in/mohit-kesar-398b251ab/",
+  },
+];
+
+export const clients = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
